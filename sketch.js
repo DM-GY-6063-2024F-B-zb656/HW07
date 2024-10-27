1f3578ba6aa0a84fe6ee8d1417cb022133edbb19
zb656 <145730190+zb656@users.noreply.github.com>
2024-10-27 13:34:38 -0400

    troubles with text size

--- sketch.js
+++ sketch.js
@@ -1,6 +1,45 @@
+let strings = "";
+
+
+function preload() {
+  strings = loadStrings("../textstring.txt");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(230);
+  frameRate(5);
 }
 
 function draw() {
+
+  string = strings[0];
+
+  let words = string.split(" ");
+
+  let xpos = 20;
+  let ypos = height/2;
+  textSize(30);
+
+  for(idx = 0; idx < words.length; idx++) {
+
+    let wordWidth = textWidth(words[idx]);
+
+    text(words[idx], xpos, ypos);
+
+    if (words[idx] == "Cathy") {
+      textSize(12);
+    } else {
+      textSize(30);
+    }
+
+    xpos = xpos + wordWidth + textWidth(" ");
+
+    let nextWordWidth = textWidth(words[idx + 1]) || 0
+
+    if (xpos > width - nextWordWidth) {
+      ypos += 40;
+      xpos = 20;
+    }
+  }
 }
